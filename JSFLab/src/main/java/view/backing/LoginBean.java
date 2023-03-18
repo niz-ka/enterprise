124ffa3d02bd684e0586e839cd1e76916e6a834a
niz-ka <kamil.niznik2000@gmail.com>
2023-03-18 14:56:43 +0100

    feat: JSF lab

---
+++ JSFLab/src/main/java/view/backing/LoginBean.java
@@ -0,0 +1,121 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package view.backing;
+
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.component.html.HtmlCommandButton;
+import jakarta.faces.component.html.HtmlSelectBooleanCheckbox;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.event.ValueChangeEvent;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ *
+ * @author kamil
+ */
+@Named(value = "loginBean")
+@RequestScoped
+public class LoginBean {
+
+    private String username;
+    private String password;
+    private HtmlSelectBooleanCheckbox acceptCheckbox;
+    private HtmlCommandButton loginButton;
+
+    /**
+     * Creates a new instance of LoginBean
+     */
+    public LoginBean() {
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String login() {
+        if (username.equals("scott") && password.equals("tiger")) {
+            FacesContext context = FacesContext.getCurrentInstance();
+            Locale.setDefault(new Locale("pl", "PL"));
+            ResourceBundle myResources = ResourceBundle.getBundle("ApplicationMessages", context.getViewRoot().getLocale());
+            String messageContent = myResources.getString("validation.oracle");
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, messageContent, messageContent);
+            context.addMessage(null, message);
+            return "index";
+        }
+
+        if (username.equals(password)) {
+            return "success";
+        } else {
+            return "failure";
+        }
+    }
+
+    /**
+     * @return the acceptCheckbox
+     */
+    public HtmlSelectBooleanCheckbox getAcceptCheckbox() {
+        return acceptCheckbox;
+    }
+
+    /**
+     * @param acceptCheckbox the acceptCheckbox to set
+     */
+    public void setAcceptCheckbox(HtmlSelectBooleanCheckbox acceptCheckbox) {
+        this.acceptCheckbox = acceptCheckbox;
+    }
+
+    /**
+     * @return the loginButton
+     */
+    public HtmlCommandButton getLoginButton() {
+        return loginButton;
+    }
+
+    /**
+     * @param loginButton the loginButton to set
+     */
+    public void setLoginButton(HtmlCommandButton loginButton) {
+        this.loginButton = loginButton;
+    }
+
+    public void activateButton(ValueChangeEvent e) {
+        if (acceptCheckbox.isSelected()) {
+            loginButton.setDisabled(false);
+        } else {
+            loginButton.setDisabled(true);
+        }
+
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.renderResponse();
+    }
+
+}
